267c18b145ed37eace933186dbd841f22c5e3580
pehvbot <mshaslam@gmail.com>
2023-03-31 20:21:51 -0700

    Update mv.sh

--- source/mv.sh
+++ source/mv.sh
@@ -1,3 +1,4 @@
 #/bin/sh
-mv bin/Debug/RackMount.* ../../KSP\ RackMount/GameData/RackMount/plugins/
+cp bin/Debug/RackMount.* ../../KSP\ RackMount/GameData/RackMount/plugins/
 cp RackMountKerbalism/bin/Debug/RackMountKerbalism.* ../../KSP\ RackMount/GameData/RackMount/plugins/
+cp RackMountKCT/bin/Debug/RackMountKCT.* ../../KSP\ RackMount/GameData/RackMount/plugins/
